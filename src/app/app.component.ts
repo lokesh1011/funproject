80e99d7d46963f4da5541e844f23646f250e2896
Lokeswara Reddy Bollavaram <lokeswara.bollavaram@accenture.com>
2023-04-13 01:15:07 +0530

    required hanges:wq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-api-project';
+  data: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.apiService.getData().subscribe(response => {
+      this.data = response;
+    });
+  }
 }
